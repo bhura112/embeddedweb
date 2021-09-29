dc23288e7b445c9e9049cce39d5f37d538f28213
Bhura Malik <rnd5@apaulinstruments.in>
2021-09-29 18:20:47 +0530

    Signed-off-by: eweb

--- mcu/index.php
+++ mcu/index.php
@@ -124,27 +124,15 @@
                     </div>  
                   </div> 
                 </a>
-
-          
                 </div>
         </section>
 
         <section class="page-container">
-         <div class="article">
-           <h4>Welcome to the World of Microcontrollers, a tiny (yet very powerful) device that changed the face of Embedded Systems. 
-               In this tutorial / article, you will learn about the Rise of Microcontrollers, few Basics of Microcontrollers, Microcontroller’s Structure and also few differences between Microprocessor and Microcontroller.
-               Before going in to the details of Basics of Microcontrollers, you need to know what a Microcontroller</h4>
-           <h2>What is Microcontroller?</h2>
-           <h4>A Microcontroller is a VLSI (Very Large Scale Integration) Integrated Circuit (IC) that contains electronic computing unit and logic unit (combinedly known as CPU), Memory (Program Memory and Data Memory), I/O Ports (Input / Output Ports) and few other components integrated on a single chip.
-           <h4>Microcontrollers are omnipresent. If a device or an application involves measuring, storing, calculating, controlling or displaying information, then device contains a Microcontroller in it. Let us see some of the areas where microcontrollers are used.
-           <h4>The biggest user of Microcontrollers is probably the Automobiles Industry. Almost every car that comes out of the assembly factory contains at least one Microcontroller for the purpose of engine control. You can find many more Microcontrollers for controlling additional systems.
-          </h4> 
-          </div>
-
           <div class="topics">
             <div class="table-of-content">
               <h3>Table Index</h3>
               <a href="#architecture">Architectures</a>
+              <a href="#packaging">Packaging</a>
               <a href="#mem">Memory </a>
               <a href="#io">Input/Output </a>
               <a href="#timer">Timer</a>
@@ -153,9 +141,19 @@
               <a href="#spi">SPI </a>
             </div>
             <div class="table-topic">
+              <div class="description">   
+                <p>A Microcontroller is a VLSI (Very Large Scale Integration) Integrated Circuit (IC) that contains electronic computing unit and logic unit (combinedly known as CPU), Memory (Program Memory and Data Memory), I/O Ports (Input / Output Ports) and few other components integrated on a single chip.</p>
+                 <img class="img-center" src="img/mcu.svg" alt="" srcset="">
+                 <p>Microcontrollers are omnipresent. If a device or an application involves measuring, storing, calculating, controlling or displaying information, then device contains a Microcontroller in it. Let us see some of the areas where microcontrollers are used.</p>
+                 <p>The biggest user of Microcontrollers is probably the Automobiles Industry. Almost every car that comes out of the assembly factory contains at least one Microcontroller for the purpose of engine control. You can find many more Microcontrollers for controlling additional systems.</p>
+                 <p> Microcontrollers are similar to Microprocessors, but they have few additional components on the same chip.</p>
+              </div>
+             
+              <!------------------------------------------------------------------------------------------------ -->
               <div id="architecture" class="table-topic-container jumptarget">
                      <div class="container-body">
                        <h2>Architectures</h2>
+                       <img src="img/arch.svg" alt="" srcset="">
                        <h5 class="fa fa-2x">Von Neumann Architecture</h5>
                        <h4>This type of architecture has common Memory for storage of data and programs.</h4>
                        <img src="img/von.png" alt="">
@@ -171,40 +169,157 @@
                             transfers and instruction fetches. This allows the CPU to fetch data and instructions at the same time.</h4>
                       </div>
               </div>
+              <div id="packaging" class="table-topic-container jumptarget">
+                     <div class="container-body">
+                       <h2>Packaging</h2>
+                       <h4>IC (Integrated Circuit) is one of the most important electronic components, due to the complex functions and uses involved, there are many types of ICs. In order to facilitate the management, IC chips were defined as various standard packages.</h4>
+
+                       <img src="img/pack.svg" alt="" srcset="">
+
+                       <h4>IC packaging indicates the dimension and shape of a chip. Chips with the same electronic parameters may have different IC packages. IC package types are mainly divided into traditional DIP dual-in-line and SMD chip package, they are soldered by different methods (Wave soldering & Reflow soldering).</h4>
+                       
+                       <!----------------- -->
+                        <h5 class="fa fa-2x">DIP (Double In-line Package)</h5>
+                        <h4>The leads are led out from both sides of the body, the materials include plastic and ceramic. DIP applications include standard logic ICs, memory LSIs, and microcomputer circuits.
+                        </h4>
+                        <img src="img/dip.svg" alt="" srcset="">
+                        <h4>The pin pitch of DIP is typically 15.2mm. Some packages with widths of 7.52mm and 10.16mm are called skinny DIP and slim DIP (narrow-body DIP). However, in most cases, it is not differentiated and is simply referred to as DIP.
+                        </h4>   
+                        
+                        <h5 class="fa fa-2x">SOP/SOIC/SO (Small Outline Package)</h5>
+                        <h4>The pins are drawn in an L shape from both sides of the body, the materials include plastic and ceramic. In addition to being used for memory LSI, SOP is also widely used in circuits such as ASSP that is not too large. The pin pitch is 1.27mm and the number of pins is from 8 to 44.</h4>
+                        <img src="img/sop.svg" alt="" srcset="">
+
+                        <h5>SOP derives some more IC packaging types, as follows:</h5>
+
+                        <h5>SOJ (Small Out-Line J-Leaded Package): J-lead small outline package</h5>
+
+                        <h5>TSOP (Thin Small Outline Package): This IC packaging is thinner than SOP with a pin pitch of 1.27mm</h5>
+
+                        <h5>SSOP (Shrink Small Outline Package): Pin pitch is 0.635mm</h5>
+
+                        <h5>TSSOP (Thin Shrink Small Outline Package): This IC package type is thinner than SOP with a pitch of 0.65mm</h5>
+
+                        <h5>QSOP (Quarter-size Small Outline Package): Pin pitch is 0.635 mm</h5>
+
+                        <h5>VSOP (Very Small Outline Package): It is smaller than QSOP and has a pitch of 0.4, 0.5 or 0.65 mm</h5>
+<!-------------------------------------------------------------------------  -->
+                        <h5 class="fa fa-2x">QFP (Quad Flat Package)</h5>
+                        <h4>The leads are led out from four sides in an L-shape, the materials are ceramic, metal and plastic, among which, plastic packaging accounts for the vast majority.
+                        </h4>
+                        <h4>The pin pitches include 1.0mm, 0.8mm, 0.65mm, 0.5mm, 0.4mm, 0.3mm and other specifications. The maximum number of pins of 0.65mm package is 304.
+                        </h4>
+                        <img src="img/qfp.svg" alt="" srcset="">
+                        <h4>Depending on the thickness of the package body, QFP currently has the following variants:
+                        </h4>
+                        <h5>LQFP (Flat Quad Flat Pack): The height of the IC packaging body is 1.4 mm</h5>
+
+                        <h5>TQFP (Thin Quad Flat Pack): Height is 1.0mm</h5>
+
+                        <h5>PQFP (Plastic Quad Flat Package): PQFP package has a small pitch and a very small pin. Generally, large-scale or very large-scale integrated circuits use this IC packaging, and the pin count is generally above 100
+                        </h5>
+                        <h5>CQFP (Ceramic Quare Flat Package): Ceramic version of PQFP</h5>
+
+                        <h5>BQFP (Quad Flat Package with Bumper): This package has protrusions (cushions) at the four corners to prevent bending deformation of the pins during shipping. The pin pitch is 0.635mm, and the number of pins is from 84 to 196
+                        </h5>
+                        
+
+                        <h5 class="fa fa-2x">QFN/LCC (Quad Flat Non-leaded Package)</h5>
+                        
+
+                        <h4>The four sides of this IC packaging are equipped with electrode contacts. Due to the absence of leads, the mounting area is smaller than QFP and the height is lower than QFP.
+                        </h4>
+                        <h4>The number of electrode contacts is generally from 14 to 100, the materials are ceramic and plastic. When there is an LCC mark, it is basically a ceramic QFN, of which the spacing of electrode contacts is 1.27 mm.
+                        </h4>
+                        <img src="img/qfn.svg" alt="" srcset="">
+                        <h4>Plastic QFN has different pin pitches of 0.65mm, 0.5mm, and 1.27mm, this package is also known as PLCC.</h4>
+
+
+                        
+
+                        <h5 class="fa fa-2x">BGA (Ball Grid Array Package)</h5>
+                        
+
+                        <h4>Spherical bumps are formed on the back surface of the printed substrate to replace the lead, and the LSI chip is mounted on the front surface of the PCB board, and then sealed by a molding resin or a potting method, the pins can exceed 200.
+                        </h4>
+                        <h4> The package body can be made smaller than QFP and the BGA does not have to worry about pin deformation problems like QFP.
+                        </h4>
+                        <img src="img/bga.svg" alt="" srcset="">
+                        <h4>The BGA package currently has the following variations:</h4>
+
+                        <h5>CBGA (Ceramic BGA)</h5>
+
+                        <h5>FBGA (Fine BGA)</h5>
+
+                        <h5>LBGA (Low-profile BGA)</h5>
+
+                        <h5>LFBGA (Low-profile Fine-pitch BGA)</h5>
+
+                        <h5>MBGA (Micro Ball Grid Array)</h5>
+
+                        <h5>MAPBGA (Moulded Array Process BGA)</h5>
+
+                        <h5>PBGA (Plastic BGA)</h5>
+
+                        <h5>TBGA (Tape BGA)</h5>
+
+                        <h5>TEPBGA (Thermally Enhanced Plastic BGA)</h5>
+
+                        <h5>UBGA (Ultra Fine BGA)</h5>
+
+                        <h5>...</h5>
+                        <!----------------------------------- -->
+                        <h5 class="fa fa-2x">CSP (Chip Scale Package)</h5>
+
+                        <h4>This IC packaging can reach close to 1:1 ratio of chip area to package. The absolute size is only 32 square millimeters, which is about 1/3 of the ordinary BGA, 1/6 of the chip area of TSOP memory. Compared to the BGA, the CSP package in the same space can increase the storage capacity by three times.
+                        </h4>
+                        <img src="img/csp.svg" alt="" srcset="">
+                        <h4>CSP is just an IC packaging standard, every package type than reach this ratio can be called CSP. Whether a PCB assembly house is able to deal with the CSP IC packaging or not reflects its assembly capabilities.
+                        </h4>
+                      </div>
+              </div>
+
               <div id="mem" class="table-topic-container jumptarget">
                      <div class="container-body">
                      <h2>Memory</h2>
+                     <p>Microcontrollers/Microprocessors are producted, essentially, with three types of memories:</p>
+                     <h5 class="fa fa-2x"> Flash memory </h5>
+                      <h4>Flash memory is ‘non-volatile’, which means that when the power is shut off, the contents of the memory are NOT lost. Flash memory is used to store the program. Flash memory can be write a lot of times (see Endurance parameter in datasheet), so it is possibile to update the microcontroller program without hardware replacements. However, do not use flash memory to store program variables because lifetime of memory will decrease very very rapidly.
+                      </h4>
+                        
+
+                    <h5 class="fa fa-2x"> RAM memory</h5>
+                      <h4>RAM (Random-Access-Memory) is ‘volatile’, which means that when the power is shut off, the contents of the memory are lost. RAM memory is used to store the variables during program execution.  RAM memory offers fast read access times. There are several types of RAM memory (SRAM, DRAM, etc.).
+                      </h4>
+                        
+
+                    <h5 class="fa fa-2x"> EEPROM memory</h5>
+                        <h4>EEPROM (Electrically Erasable Programmable Read Only Memory) is ‘non-volatile’. EEPROM memory offers slow response times both in reading and writing operations. It’s used to permanently store data like device parameters, sensors data, etc.</h4>
+                    </div>
+              </div>
+              <div id="io" class="table-topic-container jumptarget">
+                     <div class="container-body">
+                      <h2>INPUT/OUPUT PORTS</h2>
+                      <h4>A micro-controller communicates with the outside world by either</h4>
+                      <h5>setting the voltage on a pin high or low <br>or by reading the TTL-voltage level of an input pin as being either high (1) or low (0).</h5>
+                      <h4>We refer to these as input/output or IO pins.</h4>
+                      <h4>These ports serve as either inputs or outputs for the micro-controller (hence the name IO port). At a given time, however, a pin can only act as either input or output. This means that each pin has a distinct direction state.</h4>
+                      <img src="img/io.svg" alt="" srcset="">
+                      <h4>The programmer can control a pin's direction state by setting an appropriate bit in a direction register.</h4>
+                      <h4>The programmer can control a pin's logic state by setting an appropriate bit in a write register.</h4>
+                      <h4>The programmer can read a pin's logic state by getting an appropriate bit from a read register.</h4>
+                      
+                      <h5 class="fa fa-2x"> Data Direction Register</h5>
+                      
+                      <h5 class="fa fa-2x"> Data Write Register</h5>
+                      
+                      <h5 class="fa fa-2x"> Data Read Register</h5>
+
                     </div>
               </div>
             </div>
          </div>  
         </section>
-         <section class="page-container">
-          <div class="conatiner introduction">
-            
-            <p></p>
-               <p> Microcontrollers are similar to Microprocessors, but they have few additional components on the same chip.</p>
-          </div>
-
-          <div class="conatiner whatis">
-
-          <h2>What is a Microcontroller?</h2>
-          <p></p>
-
-          <p>Sometimes, a Microcontroller is also called as a Computer-on-a-Chip or a Single-Chip-Computer. Since the Microcontroller and its supporting circuitry are often embedded in the device it controls, a Microcontroller is also called as an Embedded Controller.
-          </p>
-          <p>Microcontrollers are omnipresent. If a device or an application involves measuring, storing, calculating, controlling or displaying information, then device contains a Microcontroller in it. Let us see some of the areas where microcontrollers are used.
-          </p>
-
-          <p>The biggest user of Microcontrollers is probably the Automobiles Industry. Almost every car that comes out of the assembly factory contains at least one Microcontroller for the purpose of engine control. You can find many more Microcontrollers for controlling additional systems.
-          </p>
-          <p>Consumer Electronics is another area which is loaded with Microcontrollers. Microcontrollers are a part of Digital Cameras, Video Camcorders, CD and DVD Players, Washing Machines, Ovens, etc.
-          </p>
-          <p>Microcontrollers are also used in test and measurement equipment like Multimeters, Oscilloscopes, Function Generators, etc. You can also find microcontrollers near your desktop computer like Printers, Routers, Modems, Keyboards, etc.
-          </p>
-          <p>The above definitions of the Microcontroller might seem complicated or confusing to newbies in Electronics or Embedded Systems but the concept will become clear as we move forward.
-          </p>   </div>
-         </section>
        <!-- <section class="page-section">
 
           <div class= "page-content"> 
