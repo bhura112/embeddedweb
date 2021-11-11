5124b924445af618a5be770189d24f73668b35c9
Bhura Malik <rnd5@apaulinstruments.in>
2021-11-11 14:22:15 +0530

    update1111211422

--- qt/index.php
+++ qt/index.php
@@ -14,12 +14,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://gitcdn.link/repo/bhura112/embeddedweb/main/global/headerScript.js"></script>
-    <script src="https://gitcdn.link/repo/bhura112/embeddedweb/main/global/footerScript.js"></script>
+    <script src="https://bhura112.github.io/embeddedweb/global/headerScript.js"></script>
+    <script src="https://bhura112.github.io/embeddedweb/global/footerScript.js"></script>
     
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/bhura112/embeddedweb@master/global/style.css">
+<link rel="stylesheet" href="https://bhura112.github.io/embeddedweb/global/style.css">
 
-    <link rel = "icon" href = "https://cdn.jsdelivr.net/gh/bhura112/embeddedweb@main/global/logo.png" type="image/x-icon">
+    <link rel = "icon" href = "https://bhura112.github.io/embeddedweb/global/logo.png" type="image/x-icon">
 
     <script src="https://kit.fontawesome.com/2197d61e3b.js" crossorigin="anonymous"></script>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8795837294797076"
