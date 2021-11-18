c4382d7b2335643c028564354396de50bf0530f7
Bhura Malik <rnd5@apaulinstruments.in>
2021-11-18 17:18:47 +0530

    vh

--- qt/script.js
+++ qt/script.js
@@ -6,18 +6,27 @@ var s_elemet    = document.getElementById('sticky-c');
 
 function doSomething(scrollPos) {
   var ele_rec = s_container.getBoundingClientRect();
+  var sloc = document.body.clientHeight - scrollPos;
+  console.log(sloc);
 
-  if (ele_rec.top < 200){
-    console.log("sticky");
+  if(sloc < 1500) {
+    s_elemet.style.position = "relative";
+    s_elemet.style.top = "";
+    s_elemet.style.left="";
+    s_elemet.style.margin = "";
+  }else  if (ele_rec.top < 200){
     s_elemet.style.position = "fixed";
     s_elemet.style.top = "100px";
     s_elemet.style.margin = "0px -50px";
-  }else {
+  }
+  else {
     s_elemet.style.position = "relative";
     s_elemet.style.top = "";
     s_elemet.style.left="";
     s_elemet.style.margin = "";
   }
+
+  console.log();
 }
 
 document.addEventListener('scroll', function(e) {
